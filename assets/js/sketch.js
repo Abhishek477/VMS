6d16138e12b216eebc37dda0125fed67efa72c26
Abhishek477 <abhishek18296@gmail.com>
2018-09-04 19:31:21 +0530

    Hashed password

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -3,30 +3,31 @@ var db = firebase.database();
 
 
 function dataStore(){
-  var FName =  document.getElementById('FName').value;
-  var MName =  document.getElementById('MName').value;
-  var LName =  document.getElementById('LName').value;
-  var dob =  document.getElementById('DOB').value;
-  var DLNo =  document.getElementById('DLNo').value;
-  var VRNo =  document.getElementById('VRNo').value;
-  var VManu =  document.getElementById('VManu').value;
-  var VModel =  document.getElementById('VModel').value;
-  var email =  document.getElementById('email').value;
-  var password =  document.getElementById('password').value;
-
+  var FName     =  document.getElementById('FName').value;
+  var MName     =  document.getElementById('MName').value;
+  var LName     =  document.getElementById('LName').value;
+  var dob       =  document.getElementById('DOB').value;
+  var DLNo      =  document.getElementById('DLNo').value;
+  var VRNo      =  document.getElementById('VRNo').value;
+  var VManu     =  document.getElementById('VManu').value;
+  var VModel    =  document.getElementById('VModel').value;
+  var email     =  document.getElementById('email').value;
+  var password  =  document.getElementById('password').value;
+  var hash      =  window.btoa(password);
+  
   var user = {
-    FName : FName,
-    MName : MName,
-    LName : LName,
-    DOB : dob,
-    DLNo : DLNo,
-    VRNo : VRNo,
-    VManu : VManu,
-    VModel : VModel,
-    Email : email,
-    Password : password
+    FName     : FName,
+    MName     : MName,
+    LName     : LName,
+    DOB       : dob,
+    DLNo      : DLNo,
+    VRNo      : VRNo,
+    VManu     : VManu,
+    VModel    : VModel,
+    Email     : email,
+    Password  : hash
   };
-  var ref = db.ref("Registration/" + Date.now());
+  var ref = db.ref("Registration/" + window.btoa(email));
   ref.set(user)
   .then(function() {
     console.log("Document successfully written!");
